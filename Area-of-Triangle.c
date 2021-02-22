a43b9d7b7ae9baadcdbfccd2f466d36656f99da3
chirag-ks <78857346+chirag-ks@users.noreply.github.com>
2021-02-22 21:32:01 +0530

    Update Area-of-Triangle.c

--- Area-of-Triangle.c
+++ Area-of-Triangle.c
@@ -1,21 +1,21 @@
-// WAP to calculate area of triangle using function. 
+// WAP to calculate area of triangle using functions.
 
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
- float area(float b, float h)
- {
-   float a;
-   a=(b*h)/2 ;
-   return a;
- }
+float area(float, float, float);
 int main()
 {
-   float b,h,ar;
-   printf("Enter the value of Base and Height \n");
-   scanf("%f %f",&b,&h);
-   ar = area(b,h);
-   printf("Area of the Triangle is %.2f", ar);
-   return 0;
+    float a, b, c, ar;
+    printf("Enter the length of the three sides of the Triangle.\n");
+    scanf("%f %f %f", &a, &b, &c);
+    ar=area(a, b, c);
+    printf("The area of the triangle is = %f square units\n", ar);
+    return 0;
+}
+float area(float x, float y, float z)
+{
+    float s = (x + y + z) / 2;
+    float a = sqrt(s * (s - x) * (s - y) * (s - z));
+    return a;
 }
